1cfe82ab492f917a009a9e5bb485aa6ca94943e6
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-27 14:11:55 +0000

    "Web Audio API - Oscillator Tutorial" done

---
+++ Web Audio API - Oscillator Tutorial/js/app.js
@@ -0,0 +1,45 @@
+"use strict";
+// for cross browser
+const ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+const osc = ctx.createOscillator();
+
+// osc.type = "sine";
+// osc.type = "square";
+// osc.type = "triangle";
+osc.type = "sawtooth";
+
+osc.connect(ctx.destination);
+
+osc.start();
+
+setTimeout(() => {
+osc.stop();
+}, 500);
+// osc.stop(1);
+
+const frequencyRange = document.querySelector("input");
+
+frequencyRange.addEventListener("input", (event) => {
+  console.log(event);
+  console.log(event.target);
+  console.log("event.target.value:" + event.target.value);
+  osc.frequency.value = event.target.value;
+}
+)
+
+document.getElementById("play-button").addEventListener("click", function () {
+  
+  if (ctx.state !== "running") {
+    // console.log("it's not running well");
+    // oscState = true;
+    ctx.resume();
+    // audioCtx.onstatechange = () => console.log(audioCtx.state);
+  } else {
+    // console.log("it's running");
+    // oscState = false;
+    ctx.suspend();
+    // console.log(audioCtx.state);
+    // audioCtx.onstatechange = () => console.log(audioCtx.state);
+  }
+});
