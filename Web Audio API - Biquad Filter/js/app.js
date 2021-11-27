cd87d3e5e6f86a11ec09415b67998d2cb672c75c
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-27 17:35:34 +0000

    "Web Audio API - Biquad Filter" done

---
+++ Web Audio API - Biquad Filter/js/app.js
@@ -0,0 +1,79 @@
+"use strict";
+const ctx = new(window.AudioContext || window.webkitAudioContext)();
+
+const osc = ctx.createOscillator();
+
+osc.type = 'sawtooth';
+
+const filter = ctx.createBiquadFilter();
+filter.type = 'bandpass';
+filter.frequency.value = 1500;
+filter.Q.value = 10;
+
+osc.connect(filter);
+
+filter.connect(ctx.destination);
+
+osc.start(0);
+osc.stop(1);
+
+
+
+// for cross browser
+// const ctx = new(window.AudioContext || window.webkitAudioContext)();
+
+// const osc = ctx.createOscillator();
+
+// osc.connect(ctx.destination);
+
+// console.log(ctx.state);
+// console.log(ctx);
+
+// const btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+//   const ctx = new(window.AudioContext || window.webkitAudioContext)();
+//   const osc = ctx.createOscillator();
+//   osc.connect(ctx.destination);
+//   osc.start(0);
+//   osc.stop(1);
+//   osc.onended = () => {
+//     console.log(ctx.state);
+//   }
+// })
+
+
+// const osc = ctx.createOscillator();
+
+// osc.connect(ctx.destination);
+
+// osc.frequency.value = 200;
+
+// osc.start(0);
+// osc.stop(1);
+
+// const frequencyRange = document.querySelector("input");
+
+// frequencyRange.addEventListener("input", (event) => {
+//   console.log(event);
+//   console.log(event.target);
+//   console.log("event.target.value:" + event.target.value);
+//   osc.frequency.value = event.target.value;
+// }
+// )
+
+/* document.getElementById("play-button").addEventListener("click", function () {
+
+  if (ctx.state !== "running") {
+    // console.log("it's not running well");
+    // oscState = true;
+    ctx.resume();
+    // audioCtx.onstatechange = () => console.log(audioCtx.state);
+  } else {
+    // console.log("it's running");
+    // oscState = false;
+    ctx.suspend();
+    // console.log(audioCtx.state);
+    // audioCtx.onstatechange = () => console.log(audioCtx.state);
+  }
+}); */
