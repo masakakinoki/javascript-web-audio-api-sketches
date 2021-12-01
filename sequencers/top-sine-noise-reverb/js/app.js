337ac836a25f0afcdca524adf017c6cda847e6aa
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-12-01 17:41:34 +0000

    fixed

--- sequencers/top-sine-noise-reverb/js/app.js
+++ sequencers/top-sine-noise-reverb/js/app.js
@@ -13,10 +13,82 @@ let osc = audioCtx.createOscillator();
 let topSineVolume = audioCtx.createGain();
 topSineVolume.gain.value = 0.1;
 
+//freeverb copy and paste from index.js
+import CompositeAudioNode from './freeverb/composite-audio-node.js';
+import mergeParams from './freeverb/merge-params.js';
+import LowpassCombFilter from './freeverb/low-pass-comb-filter.js';
+
+// Freeverb params defined by Mr. Shroeder
+const SAMPLE_RATE = 44100;
+const COMB_FILTER_TUNINGS = [1557, 1617, 1491, 1422, 1277, 1356, 1188, 1116];
+const ALLPASS_FREQUENCES = [225, 556, 441, 341];
+
+const getAllPass = (audioCtx, freq) => {
+  const allPass = audioCtx.createBiquadFilter();
+  allPass.type = 'allpass';
+  allPass.frequency.value = freq;
+  return allPass;
+};
+
+class Freeverb extends CompositeAudioNode {
+
+  get wetGain () {
+    return this._wet.gain;
+  }
+
+  get dryGain () {
+    return this._dry.gain;
+  }
+
+  get roomSize() {
+    return mergeParams(this._combFilters.map(comb => comb.resonance));
+  }
+
+  get dampening() {
+    return mergeParams(this._combFilters.map(comb => comb.dampening));
+  }
+
+  constructor (audioCtx, options) {
+    super(audioCtx, options);
+    const {roomSize: resonance, dampening, wetGain, dryGain} = options;
+
+    this._wet = audioCtx.createGain();
+    this._wet.gain.setValueAtTime(wetGain, audioCtx.currentTime);
+    this._dry = audioCtx.createGain();
+    this._dry.gain.setValueAtTime(dryGain, audioCtx.currentTime);
+    this._combFilters = COMB_FILTER_TUNINGS
+    .map(delayPerSecond => delayPerSecond / SAMPLE_RATE)
+    .map(delayTime => new LowpassCombFilter(audioCtx, {dampening, resonance, delayTime}));
+
+    const merger = audioCtx.createChannelMerger(2);
+    const splitter = audioCtx.createChannelSplitter(2);
+    const combLeft = this._combFilters.slice(0, 4);
+    const combRight = this._combFilters.slice(4);
+    const allPassFilters = ALLPASS_FREQUENCES.map(freq => getAllPass(audioCtx, freq));
+
+    //connect all nodes
+    this._input.connect(this._wet).connect(splitter);
+    this._input.connect(this._dry).connect(this._output);
+    combLeft.forEach(comb => {
+      splitter.connect(comb, 0).connect(merger, 0, 0);
+    });
+    combRight.forEach(comb => {
+      splitter.connect(comb, 1).connect(merger, 0, 1);
+    });
+    merger
+    .connect(allPassFilters[0])
+    .connect(allPassFilters[1])
+    .connect(allPassFilters[2])
+    .connect(allPassFilters[3])
+    .connect(this._output);
+  }
+}
+
+
 //create an instance
 // const Freeverb = require('Freeverb')
-import Freeverb from　'./freeverb';
-const opts = { dampening: 3000, roomSize: 0.7, dryGain: 0.2, wetGain: 0.8 }
+// import Freeverb from　'./freeverb';
+const opts = { dampening: 3000, roomSize: 0.7, dryGain: 0.1, wetGain: 1.0 }
 const freeverb = new Freeverb(audioCtx, opts)
 
 function playTopSine(time, playing) {
